13e8b135316a8dfdb408dc38696594b26a5b1305
remorses <beats.by.morse@gmail.com>
2023-02-03 19:45:58 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# framer-motion-visualizer
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-react-tailwind-template-zhagyu)
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import logo from './logo.svg';
+import './index.css';
+import classNames from 'classnames';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div className=""></div>;
+}
+
+export default App;
